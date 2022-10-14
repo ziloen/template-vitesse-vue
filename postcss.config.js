14f8ccff0bd67a42ef7b45494d2666410e88b5fd
N <34268416+ziloen@users.noreply.github.com>
2022-10-15 01:57:46 +0800

    chore

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: {
-    // Options https://github.com/csstools/postcss-plugins/tree/main/plugin-packs/postcss-preset-env#options
-    'postcss-preset-env': {
-      stage: 2
-      // importFrom: ["path/to/file.css"]
-    }
-  }
-}
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import Pages from 'vite-plugin-pages'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Unocss from 'unocss/vite'
+import PostcssPresetEnv from 'postcss-preset-env'
 
 export default defineConfig({
   resolve: {
@@ -46,6 +47,12 @@ export default defineConfig({
     // see unocss.config.ts for config
     Unocss()
   ],
+
+  css: {
+    postcss: {
+      plugins: [PostcssPresetEnv({ stage: 2 })]
+    }
+  },
 
   // https://github.com/vitest-dev/vitest
   test: {
