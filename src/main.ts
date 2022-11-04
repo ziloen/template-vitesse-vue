71abaca6feada4e99c0e52ef642c960738937bf3
N <34268416+ziloen@users.noreply.github.com>
2022-11-04 17:18:25 +0800

    chore: format

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
+import routes from 'virtual:generated-pages'
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import routes from 'virtual:generated-pages'
 import App from './App.vue'
 
 import '@unocss/reset/tailwind.css'
-import './styles/main.css'
 import 'uno.css'
 import './styles/common.scss'
+import './styles/main.css'
 
-const app = createApp(App)
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
-app.use(router)
-app.mount('#app')
+
+createApp(App)
+  .use(router)
+  .mount('#app')
