8ad5def940e82f4b0d6403c691765f3c6f2e4229
N <34268416+ziloen@users.noreply.github.com>
2022-12-01 16:26:33 +0800

    chore

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import App from './App.vue'
 
 import '@unocss/reset/tailwind.css'
 import 'uno.css'
-import './styles/common.scss'
+import './styles/atom.scss'
 import './styles/main.css'
 
 const router = createRouter({
