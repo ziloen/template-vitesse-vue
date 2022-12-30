0da8a2479f6377e1da763831c99e91690d2d803d
N <34268416+ziloen@users.noreply.github.com>
2022-12-30 12:05:19 +0800

    chore: use naive-ui dark theme

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <n-message-provider>
-    <main class="font-sans px-4 py-10 text-center text-gray-700 dark:text-gray-200">
-      <router-view />
-      <!-- <Footer /> -->
-    </main>
-  </n-message-provider>
+  <n-config-provider :theme="darkTheme">
+    <n-message-provider>
+      <main class="font-sans px-4 py-10 text-center text-gray-700 dark:text-gray-200">
+        <router-view />
+        <!-- <Footer /> -->
+      </main>
+    </n-message-provider>
+  </n-config-provider>
 </template>
 
 
 <script setup lang="ts">
-import { NMessageProvider } from 'naive-ui'
+import { NMessageProvider, NConfigProvider, darkTheme } from 'naive-ui'
 </script>
