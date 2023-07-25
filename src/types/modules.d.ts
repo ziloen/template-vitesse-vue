0e0f8306a40a40fe9037865469cee99722063f08
N <34268416+ziloen@users.noreply.github.com>
2023-07-25 14:41:21 +0800

    chore: i18n types

--- src/types/modules.d.ts
+++ src/types/modules.d.ts
@@ -2,7 +2,7 @@
 
 
 // 修正 axios 返回类型
-declare module "axios" {
+declare module 'axios' {
   interface AxiosInstance {
     <T = any>(config: AxiosRequestConfig): Promise<T>
     <T = any>(url: string, config?: AxiosRequestConfig): Promise<T>
@@ -26,6 +26,14 @@ declare module "axios" {
       config?: AxiosRequestConfig
     ): Promise<T>
   }
+}
+
+
+import type EN from '../../locales/en.json'
+
+declare module 'vue-i18n' {
+  type ENJSON = typeof EN
+  export interface DefineLocaleMessage extends ENJSON { }
 }
 
 export { }
