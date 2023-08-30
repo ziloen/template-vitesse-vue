26046c4ed9176516462814db5d79e62e85f250bd
N <34268416+ziloen@users.noreply.github.com>
2023-08-30 17:46:26 +0800

    chore: update vue-i18n

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import './styles/main.css'
 import messages from '@intlify/unplugin-vue-i18n/messages'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
-import { createI18n } from 'vue-i18n'
+import { CompileError, MessageCompiler, MessageContext, createI18n } from 'vue-i18n'
 import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from 'vue-router/auto/routes'
 import App from './App.vue'
@@ -17,15 +17,30 @@ const router = createRouter({
   routes
 })
 
+const messageCompiler: MessageCompiler = (
+  message,
+  { key, onCacheKey, locale, onError, warnHtmlMessage }
+) => {
+  if (typeof message === 'string') {
+
+    return (ctx: MessageContext) => {
+      return ctx.values
+    }
+  } else {
+    /**
+     * for AST.
+     * If you would like to support it,
+     * You need to transform locale mesages such as `json`, `yaml`, etc. with the bundle plugin.
+     */
+    onError && onError(new Error('not support for AST') as CompileError)
+    return () => key
+  }
+}
+
 const i18n = createI18n({
   legacy: false,
   locale: 'en',
-  // custom formatter is removed.
-  // formatter: {
-  //   interpolate(message, values) {
-  //     return [message]
-  //   }
-  // },
+  // messageCompiler,
   fallbackLocale: 'en',
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   messages: messages!,
