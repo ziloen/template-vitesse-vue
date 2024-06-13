967a038f6f46cbb58bf67414b975e2bbb9433cc8
N <34268416+ziloen@users.noreply.github.com>
2024-06-13 18:59:24 +0800

    Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -8,5 +8,15 @@ export default [
       'volar.config.js',
       "src/pages/jsx/sfc.vue"
     ]
+  },
+  {
+    files: ["**/*.vue"],
+    rules: {
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-argument": "off",
+      "@typescript-eslint/no-unsafe-return": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
+    }
   }
 ]
